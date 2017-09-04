310e6851125632eae199382917213429302382dd
daniel-san <dlsbdaniel@gmail.com>
2017-09-04 19:18:25 -0300

    Adicionando classes de modelo básicas e controladores

---
+++ app/Http/Controllers/CreativesController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Creatives;
+use Illuminate\Http\Request;
+
+class CreativesController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/WidgetsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Widgets;
+use Illuminate\Http\Request;
+
+class WidgetsController extends Controller
+{
+    //
+}
