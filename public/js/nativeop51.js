5f5f09bef9e895e86acc9a37b07c93c404e2011e
Rummenigge Maia <rummeniggemaia@gmail.com>
2018-04-27 16:42:00 -0300

    Adicionado funcao de cookies nos scripts

--- public/js/nativeop51.js
+++ public/js/nativeop51.js
@@ -2,11 +2,15 @@
 	if(typeof jQuery == 'undefined'){
 		var script = document.createElement( "script" );
 		script.type = "text/javascript";
-		script.src = "http://localhost/native-optimize/public/js/jquery-3.2.1.min.js";
+		script.src = "https://app.ads4xxx.com/js/jquery-3.2.1.min.js";
 		script.onload = function () {
 			$('head').append($('<script>', {
 				type:'text/javascript',
-				src:'http://localhost/native-optimize/public/js/nativeop51.min.js'
+				src:'https://app.ads4xxx.com/js/nativeop51.min.js'
+			}));
+                        $('head').append($('<script>', {
+				type:'text/javascript',
+				src:'https://app.ads4xxx.com/js/js-cookie.js'
 			}));
 		};
 		document.getElementsByTagName("head")[0].appendChild(script);
