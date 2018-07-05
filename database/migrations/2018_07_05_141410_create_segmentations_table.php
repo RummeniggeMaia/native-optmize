0036b0f4565d850d4c0eed9622bbe08011119d83
daniel-san <dlsbdaniel@gmail.com>
2018-07-05 15:23:17 -0300

    Adicionando tabela de segmentações

---
+++ database/migrations/2018_07_05_141410_create_segmentations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSegmentationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('segmentations', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('device');
+            $table->string('country');
+            $table->timestamps();
+        });
+
+        Schema::table('segmentation', function (Blueprint $table){
+            $table->bigInteger('campaingn_id')->unsigned();
+            $table->foreign('campaingn_id')->references('id')->on('campaingns')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('segmentations');
+    }
+}
